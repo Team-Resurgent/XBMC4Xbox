5d2f76073eefda35844f7ca3eaaa9449fcad8d6d
EqUiNoX <equinox.teamresurgent@gmail.com>
2024-08-12 20:16:49 -0700

    Fixed: comment

--- xbmc/URL.cpp
+++ xbmc/URL.cpp
@@ -640,7 +640,7 @@ bool CURL::IsFullPath(const CStdString &url)
 {
   if (url.size() && url[0] == '/') return true;     //   /foo/bar.ext
   if (url.Find("://") >= 0) return true;                 //   foo://bar.ext
-  if (url.Find(":\\") >= 0) return true; //   HDD-0:\foo\bar\bar.ext
+  if (url.Find(":\\") >= 1) return true; //   HDD0-C:\foo\bar\bar.ext
   return false;
 }
 
